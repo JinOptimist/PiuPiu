41e5042a3add270e6e5a97edbd8b074da12b3d9d
Pavel Lvou <88jin@mail.ru>
2024-07-20 02:32:19 +0300

    Remove old way to generate maze

--- 3DMaze/Assets/Scripts/Smile/MazeBuilder.cs
+++ 3DMaze/Assets/Scripts/Smile/MazeBuilder.cs
@@ -1,14 +1,17 @@
-using Assets.Scripts.Smile.Maze;
+using MazeGenerator;
+using MazeGenerator.Models.MazeModels;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using WallType = MazeGenerator.Models.MazeModels.Wall;
 
 public class MazeBuilder : MonoBehaviour
 {
     public GameObject Wall;
     public GameObject Stair;
 
-    public MazeGenerator MazeGenerator;
-
     private const int WALL_SIZE = 4;
+    private const int HALF_WALL_SIZE = WALL_SIZE / 2;
 
     // will set on Start
     private int zMargin = -1 * WALL_SIZE;
@@ -16,18 +19,19 @@ public class MazeBuilder : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var maze = MazeGenerator.GenerateMaze();
-        zMargin = -1 * (maze.Length + 1) * WALL_SIZE;
+        var generator = new Generator();
+        var maze = generator.Generate(5, 4, 3, 42);
+        zMargin = -1 * (maze.Width + 1) * WALL_SIZE;
         BuildMaze(maze);
     }
 
-    private void BuildMaze(MazeCube maze)
+    private void BuildMaze(Maze maze)
     {
         for (int z = 0; z < maze.Height; z++)
         {
-            for (int y = 0; y < maze.Length; y++)
+            for (int y = 0; y < maze.Width; y++)
             {
-                for (int x = 0; x < maze.Width; x++)
+                for (int x = 0; x < maze.Length; x++)
                 {
                     BuildRoom(maze[x, y, z]);
                 }
@@ -38,56 +42,55 @@ public class MazeBuilder : MonoBehaviour
     private void BuildRoom(Cell cell)
     {
         var wall = cell.Wall;
-        if (wall.HasFlag(WallTypes.North))
+        if (wall.HasFlag(WallType.North))
         {
-            BuildWallNorthSouth(cell.X, cell.Y + 1);
+            BuildWallNorthSouth(cell.X, cell.Y + 1, cell.Z);
         }
-        if (wall.HasFlag(WallTypes.East))
+        if (wall.HasFlag(WallType.East))
         {
-            BuildWallEastWest(cell.X + 1, cell.Y);
+            BuildWallEastWest(cell.X + 1, cell.Y, cell.Z);
         }
-        if (wall.HasFlag(WallTypes.South))
+        if (wall.HasFlag(WallType.South))
         {
-            BuildWallNorthSouth(cell.X, cell.Y);
+            BuildWallNorthSouth(cell.X, cell.Y, cell.Z);
         }
-        if (wall.HasFlag(WallTypes.West))
+        if (wall.HasFlag(WallType.West))
         {
-            BuildWallEastWest(cell.X, cell.Y);
+            BuildWallEastWest(cell.X, cell.Y, cell.Z);
         }
 
-        //if (wall.HasFlag(WallTypes.StairToNorth))
+        //if (wall.HasFlag(WallType.StairToNorth))
         //{
         //    BuildStair(cell.X, cell.Y);
         //}
 
-        //if (!wall.HasFlag(WallTypes.StairToNorth)
-        //    && !wall.HasFlag(WallTypes.StairToEast)
-        //    && !wall.HasFlag(WallTypes.StairToSouth)
-        //    && !wall.HasFlag(WallTypes.StairToWest))
+        //if (!wall.HasFlag(WallType.StairToNorth)
+        //    && !wall.HasFlag(WallType.StairToEast)
+        //    && !wall.HasFlag(WallType.StairToSouth)
+        //    && !wall.HasFlag(WallType.StairToWest))
         //{
         //    BuildRoof(cell.X, cell.Y);
         //}
     }
 
-    private void BuildWallEastWest(int x, int y)
+    private void BuildWallEastWest(int x, int y, int z)
     {
         var wall = Instantiate(Wall);
         wall.transform.position = new Vector3(
             x * WALL_SIZE,
-            WALL_SIZE / 2,
+            z * WALL_SIZE + WALL_SIZE / 2,
             y * WALL_SIZE + zMargin);
     }
 
-    private void BuildWallNorthSouth(int x, int y)
+    private void BuildWallNorthSouth(int x, int y, int z)
     {
         var wall = Instantiate(Wall);
         wall.transform.Rotate(0, 90, 0);
-
-        var half = WALL_SIZE / 2;
+        
         wall.transform.position = new Vector3(
-            x * WALL_SIZE + half,
-            half,
-            (y - 1) * WALL_SIZE + half + zMargin);
+            x * WALL_SIZE + HALF_WALL_SIZE,
+            z * WALL_SIZE + WALL_SIZE / 2,
+            (y - 1) * WALL_SIZE + HALF_WALL_SIZE + zMargin);
     }
 
     private void BuildRoof(int x, int y)
